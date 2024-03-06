3e82d127431bd9afad9db16c47b5c659610323dc
Mahmed1109 <150253069+Mahmed1109@users.noreply.github.com>
2024-03-06 12:08:36 +0000

    Add files via upload

---
+++ Portfolioweb/script.js
@@ -0,0 +1,14 @@
+const javagit = document.getElementById('javagit');
+javagit.addEventListener('click', () => {
+    window.open('https://github.com/Mahmed1109/JSbootcampproject', '_blank');
+});
+
+const rpsgit = document.getElementById('rpsgit');
+rpsgit.addEventListener('click', () => {
+    window.open('https://github.com/Mahmed1109/JSbootcampproject', '_blank');
+});
+
+const pygit = document.getElementById('pygit');
+pygit.addEventListener('click', () => {
+    window.open('https://github.com/Mahmed1109/PythonSQLDatabase', '_blank');
+});
